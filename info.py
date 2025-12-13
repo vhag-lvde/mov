c419d86af4d56615aff269515224dc3348be0e07
Govind Panchal <gulabsinghrajput008@gmail.com>
2025-12-13 14:25:38 +0530

    Update info.py

--- info.py
+++ info.py
@@ -76,8 +76,8 @@ STAR_PREMIUM_PLANS = {
 # MongoDB Configuration
 # ============================
 DATABASE_URI = environ.get('DATABASE_URI', "")  # MongoDB URI for the database
-DATABASE_NAME = environ.get('DATABASE_NAME', "Cluster0") # Database name (default: cluster)
-COLLECTION_NAME = environ.get('COLLECTION_NAME', 'dreamcinezone_files') # Collection name (default: dreamcinezone_files)
+DATABASE_NAME = environ.get('DATABASE_NAME', "TELEGRAM_BOT_INFO") # Database name (default: cluster)
+COLLECTION_NAME = environ.get('COLLECTION_NAME', 'tele_data') # Collection name (default: dreamcinezone_files)
 
 # If MULTIPLE_DB Is True Then Fill DATABASE_URI2 Value Else You Will Get Error.
 MULTIPLE_DB = is_enabled(os.environ.get('MULTIPLE_DB', "False"), False) # Type True For Turn On MULTIPLE DB FUNTION 
