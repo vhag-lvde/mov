a613a07c81d124809228f12d6ea30a7d6caf8774
Govind Panchal <gulabsinghrajput008@gmail.com>
2025-12-16 18:33:39 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -456,7 +456,17 @@ async def stream_buttons(user_id: int, file_id: str):
             ]
     else:
         return [[InlineKeyboardButton('📌 ᴊᴏɪɴ ᴜᴘᴅᴀᴛᴇꜱ ᴄʜᴀɴɴᴇʟ 📌', url=UPDATE_CHNL_LNK)]]
+        
+@Client.on_message(filters.command("cleargroups") & filters.user(ADMINS))
+async def clear_groups(_, message):
+    try:
+        await db.grp.delete_many({})
+        await db.settings_col.delete_many({})
+        await message.reply_text("✅ All group data cleared successfully.")
+    except Exception as e:
+        await message.reply(f"❌ Error clearing group: {e}")
     
+
 @Client.on_message(filters.command('logs') & filters.user(ADMINS))
 async def log_file(bot, message):
     """Send log file"""
